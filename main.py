51ce95866a7d370473ebcc59e5f27b31769f6346
Ama_hocam <97902506+amahocam@users.noreply.github.com>
2022-05-24 15:56:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ async def _(client, message):
 
 # Dc Komutu İcin Olan Buttonlar
 def d_or_s(user_id):
-	BUTTON = [[InlineKeyboardButton(text="? Doğruluk", callback_data = " ".join(["d_data",str(user_id)]))]]
+	BUTTON = [[InlineKeyboardButton(text="✅ Doğruluk", callback_data = " ".join(["d_data",str(user_id)]))
+                   InlineKeyboardButton(text="💪 Cesaret", callback_data = " ".join(["c_data",str(user_id)]))]]
 	BUTTON += [[InlineKeyboardButton(text="🔞 +18 Soru", callback_data = " ".join(["s_data",str(user_id)]))]]
 	return InlineKeyboardMarkup(BUTTON)
 
@@ -66,14 +67,15 @@ async def _(client, message):
 async def _(client, callback_query):
 	d_soru=random.choice(D_LİST) # Random Bir Doğruluk Sorusu Seçelim
 	s_soru=random.choice(S_LİST) # Random Bir Cesaret Sorusu Seçelim
+        c_soru=random.choice(C_LİST)
 	user = callback_query.from_user # Kullanıcın Kimliğini Alalım
 
-	s_q_d, user_id = callback_query.data.split() # Buttonlarımızın Komutlarını Alalım
+	s_q_d_q_c, user_id = callback_query.data.split() # Buttonlarımızın Komutlarını Alalım
 
 	# Sorunun Sorulmasını İsteyen Kişinin Komutu Kullanan Kullanıcı Olup Olmadığını Kontrol Edelim
 	if str(user.id) == str(user_id):
 		# Kullanıcının Doğruluk Sorusu İstemiş İse Bu Kısım Calışır
-		if s_q_d == "d_data":
+	    if s_q_d_q_c == "d_data":
 			await callback_query.answer(text="Doğruluk Sorusu İstediniz", show_alert=False) # İlk Ekranda Uyarı Olarak Gösterelim
 			await client.delete_messages(
 				chat_id=callback_query.message.chat.id,
@@ -82,12 +84,20 @@ async def _(client, callback_query):
 			await callback_query.message.reply_text("**{user} Doğruluk Sorusu İstedi:** __{d_soru}__".format(user=user.mention, d_soru=d_soru)) # Sonra Kullanıcıyı Etiketleyerek Sorusunu Gönderelim
 			return
 
-		if s_q_d == "s_data":
+	    if s_q_d_q_c == "s_data":
 			await callback_query.answer(text="🔞 +18 Sorusu İstediniz", show_alert=False)
 			await client.delete_messages(
 				chat_id=callback_query.message.chat.id,
 				message_ids=callback_query.message.message_id)
 			await callback_query.message.reply_text("**{user} 🔞 +18 Sorusu İstedi:** __{s_soru}__".format(user=user.mention, s_soru=s_soru))
+			return
+
+             if s_q_d_q_c == "c_data":
+			await callback_query.answer(text="💪 Cesaret Sorusu İstediniz", show_alert=False)
+			await client.delete_messages(
+				chat_id=callback_query.message.chat.id,
+				message_ids=callback_query.message.message_id)
+			await callback_query.message.reply_text("**{user} 💪 Cesaret Sorusu İstedi:** __{c_soru}__".format(user=user.mention, c_soru=c_soru))
 			return
 
 
